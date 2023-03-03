5340f91fb20c634b264f899046074196031240c2
Ayora léa <ayora.lea11@gmail.com>
2023-03-03 10:43:03 +0100

    03/03/2023

--- carte.php
+++ carte.php
@@ -7,8 +7,8 @@ require('config2.php');
     <title>Carte Quai antique</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="style.css">
   </head>
