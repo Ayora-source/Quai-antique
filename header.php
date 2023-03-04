a2f9c6685bfd294b897380edfe7809f80169cbd6
Ayora léa <ayora.lea11@gmail.com>
2023-03-04 12:48:10 +0100

    header

--- header.php
+++ header.php
@@ -1,29 +1,17 @@
-<!-- <div class="p-2 m-0 border-0 bd-example">
-  <nav class="nav nav-pills flex-column flex-sm-row">
-    <a class="flex-sm-fill text-sm-center nav-link " href="index.php"><font class="header">Accueil</font></a>
-    <a class="flex-sm-fill text-sm-center nav-link " href="carte.php"><font class="header">A la carte</font></a>
-    <a class="flex-sm-fill text-sm-center nav-link " href="reservation.php"><font class="header">Réservation</font></a>
-    <a class="flex-sm-fill text-sm-center nav-link " href="register.php"><font class="header">S'identifier</font></a>
-  </nav>
-</div>
- -->
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Quai Antique</a>
+    <a class="navbar-brand" style="margin-right: 100px; font-weight: bolder;"  href="#">Quai Antique</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
-        <a class="nav-link" href="carte.ph">A la carte</a>
-        <a class="nav-link" href="reservation.php">Réservation</a>
-        <a class="nav-link" href="reservation.php">S'identifier</a>
+        <a class="nav-link header" href="index.php">Accueil</a>
+        <a class="nav-link header" href="carte.php">Notre Carte</a>
+        <a class="nav-link header" href="reservation.php">Réservation</a>
+        <a class="nav-link header" href="register.php">S'identifier</a>
       </div>
     </div>
   </div>
 </nav>
-
-
-   
 
